a2c2650c35c61efbebbb80d97809be510bfc5a1c
ritikkumar352 <krritik2002@gmail.com>
2026-04-28 09:25:59 +0530

    project setup

---
+++ src/main/java/com/ritik/game/TilesApplication.java
@@ -0,0 +1,13 @@
+package com.ritik.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ritik/game/TilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ritik.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
